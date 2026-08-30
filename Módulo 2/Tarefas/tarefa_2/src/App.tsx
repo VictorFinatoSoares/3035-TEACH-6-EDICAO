cd79d23d3c1deb1918499a65e90cf7045ff12d81
VictorFinatoSoares <victor.finattosoares@gmail.com>
2026-08-29 21:20:02 -0300

    reviewed previous tasks

--- Módulo 2/Tarefas/tarefa_2/src/App.tsx
+++ Módulo 2/Tarefas/tarefa_2/src/App.tsx
@@ -1,45 +1,55 @@
-import React, {useState} from 'react';
-import list from './data/filmes.json';
-import './App.css';
+import React, { useState } from "react";
+import list from "./data/filmes.json";
+import "./App.css";
 
 function App() {
-  const [filmeBuscado, setFilmeBuscado] = useState('');
+  const [filmeBuscado, setFilmeBuscado] = useState("");
 
-  const filmesFiltrados = list.filter(filme =>
-    filme.nome.toLowerCase().includes(filmeBuscado.toLowerCase())
+  const filmesFiltrados = list.filter((filme) =>
+    filme.nome.toLowerCase().includes(filmeBuscado.toLowerCase()),
   );
 
   return (
     <div>
-      <input placeholder='Buscar filmes' value={filmeBuscado} onChange={f => setFilmeBuscado(f.target.value)}></input>
-        
+      <input
+        placeholder="Buscar filmes"
+        value={filmeBuscado}
+        onChange={(f) => setFilmeBuscado(f.target.value)}
+      ></input>
+
       {filmesFiltrados.length === 0 ? ( // Serve pra saber se a lista de filmes filtrados encontrou um filme
         <h1>Nenhum filme encontrado!</h1> // Caso não encontrou
-      ): // Caso encontrou ao menos um filme
+      ) : (
+        // Caso encontrou ao menos um filme
         <table>
           <thead>
             <tr>
-              <th>ID</th> 
+              <th>ID</th>
               <th>Nome</th>
               <th>Gênero</th>
               <th>Capa</th>
             </tr>
           </thead>
-          <tbody> 
+          <tbody>
             {/*Cria uma linha pra cada filme encontrado, passando as informações (nome, id etc em td)*/}
             {filmesFiltrados.map((filme) => (
-              <tr key = {filme.id}>
+              <tr key={filme.id}>
                 <td>{filme.id}</td>
                 <td>{filme.nome}</td>
                 <td>{filme.genero}</td>
                 <td>
                   {/* Passa o link da imagem no parâmetro src da tag img pra exibir a imagem */}
-                  <img className = 'capas' src = {filme.imagem} alt = {filme.nome}></img>
+                  <img
+                    className="capas"
+                    src={filme.imagem}
+                    alt={filme.nome}
+                  ></img>
                 </td>
               </tr>
             ))}
           </tbody>
-        </table>}
+        </table>
+      )}
     </div>
   );
 }
